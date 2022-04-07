7530ba3bb4f836165af8d0a07f29264b63a579d1
Charles Vardeman <charles.vardeman@gmail.com>
2022-04-07 15:48:28 -0400

    Updated release build script and information object test case

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cmd='pdm run python ./__pypackages__/3.9/lib/rdfx/rdfx_cli.py'
-files='./modules/common/event.ttl ./modules/common/metadata.ttl'
-ONTOLOGY=ontology.ttl
+files='./modules/common/event.ttl ./modules/common/informationobject.ttl ./modules/common/metadata.ttl ./modules/common/site.ttl ./modules/common/system.ttl'
+shapefiles='./modules/common/event.ttl ./modules/common/informationobject.shacl.ttl ./modules/common/metadata.shacl.ttl ./modules/common/site.shacl.ttl ./modules/common/system.shacl.ttl'
+ONTOLOGY=moo.ttl
+SHACL=moo.shacl.ttl
 
 VERSION=` grep -i versionInfo modules/common/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
 
@@ -11,4 +13,7 @@ if [ -f "$ONTOLOGY" ]; then
 fi
 
 $cmd merge $files -f ttl -o ./
-mv merged.ttl moo.ttl
+mv merged.ttl $ONTOLOGY
+
+$cmd merge $shapefiles -f ttl -o ./
+mv merged.ttl $SHACL
