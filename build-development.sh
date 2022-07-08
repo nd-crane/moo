81b90af3f2a805214d04529c6f9197a345a212c3
Charles Vardeman <charles.vardeman@gmail.com>
2022-07-08 08:57:55 -0400

    Updating development build system

--- build-development.sh
+++ build-development.sh
@@ -6,10 +6,10 @@ mergecmd="pdm run python ${pdmdir}/lib/rdfx/rdfx_cli.py"
 pylodecmd="pdm run pylode"
 files=$(cat tests/modules.txt | awk -F, '{print $2}')
 shapefiles=$(cat tests/shapes.txt | awk -F, '{print $2}')
-ONTOLOGY=drone
-SHACL=drone.shacl
+ONTOLOGY=moo
+SHACL=moo.shacl
 
-VERSION=` grep -i versionInfo modules/core/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
+VERSION=` grep -i versionInfo modules/common/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
 
 if [ -f "./development/ontology/merged.ttl" ]; then
     rm ./development/ontology/merged.ttl
@@ -23,9 +23,10 @@ $mergecmd merge $files -f ttl -o ./development/ontology
 #echo "Generating HTML SHACL Shapes Documentation"
 # $pylodecmd ./development/${SHACL}.ttl -o ./development/${SHACL}.html
 
-echo "Generating HTML Ontology Documentation"
-docker run -ti --rm   -v `pwd`/development/ontology:/usr/local/widoco/in   -v `pwd`/development/ontology:/usr/local/widoco/out/doc   dgarijo/widoco -ontFile in/merged.ttl -outFolder out -rewriteAll -webVowl 
+
 # Clean up temp merged file
 if [ -f "./development/ontology/merged.ttl" ]; then
+    echo "Generating HTML Ontology Documentation"
+    docker run -ti --rm   -v `pwd`/development/ontology:/usr/local/widoco/in   -v `pwd`/development/ontology:/usr/local/widoco/out/doc   dgarijo/widoco -ontFile in/merged.ttl -outFolder out -rewriteAll -webVowl 
     rm ./development/ontology/merged.ttl
 fi
