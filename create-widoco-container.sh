6d77210a563f97d07e61e65ccd506c617de2a06b
trwiley <taylor.r.wiley@gmail.com>
2022-08-01 13:36:59 -0400

    Fixed syntax error in widoco creation script

--- create-widoco-container.sh
+++ create-widoco-container.sh
@@ -1,3 +1,4 @@
 # NOTE: it is recommended that this script is NOT run inside of the MOO folder.
 git clone https://github.com/dgarijo/Widoco.git
-cddocker build -t dgarijo/widoco .
+cd Widoco
+docker build -t dgarijo/widoco .
